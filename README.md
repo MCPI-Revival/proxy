0515d072847caa4ec9827934a55fec0d93e9de48
Alvarito050506 <donfrutosgomez@gmail.com>
2020-04-23 21:37:40 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Proxy
+A Minecraft Pi Proxy that allow to players to connect to remote servers.
+
+## Getting started
+### Prerequisites
+To use the proxy you need to have `Python >= 3.7.x` pre-installed.
+
+### Installation
+To install the proxy, download or clone the repository:
+```shell
+git clone https://github.com/MCPI-Devs/proxy.git
+```
+There are no aditional requiriments.
+
+## Usage
+Run the `proxy.py` file in the root of the repo as
+```
+proxy.py src_addr [src_port [dst_port]]
+```
+Where `src_addr` is a valid internet address and `src_port` and `dst_port` are valid internet ports.
+
+## Licensing
+All the code of this project is licensed under the [GNU General Public License version 2.0](https://github.com/Alvarito050506/MCPIL/blob/master/LICENSE) (GPL-2.0).
+
+All the documentation of this project is licensed under the [Creative Commons Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/) (CC BY-SA 4.0) license.
+
+![CC BY-SA 4.0](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)
---
+++ proxy.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  __init__.py
+#  
+#  Copyright 2020 Alvarito050506 <donfrutosgomez@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; version 2 of the License.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import sys
+import socket
+
+class Proxy:
+	def __init__(self):
+		self.__options = {
+			"src_addr": None,
+			"src_port": 19132,
+			"dst_port": 19133
+		};
+
+	def set_option(self, name, value):
+		if name in self.__options:
+			self.__options[name] = value;
+		else:
+			raise NameError(name);
+		return self.__options;
+
+	def get_options(self):
+		return self.__options;
+
+	def run(self):
+		dst_addr = ("0.0.0.0", self.__options["dst_port"]);
+		src_addr = (self.__options["src_addr"], self.__options["src_port"]);
+		client_addr = None;
+		self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP);
+		self.__socket.bind(dst_addr);
+
+		while True:
+			data, addr = self.__socket.recvfrom(4096);
+			if addr == src_addr:
+				self.__socket.sendto(data, client_addr);
+			else:
+				if client_addr is None or client_addr[0] == addr[0]:
+					client_addr = addr;
+				self.__socket.sendto(data, src_addr);
+		return 0;
+
+if __name__ == '__main__':
+	args = sys.argv;
+	if len(args) < 2:
+		print("Error: You must provide a source address.");
+		print("Usage: " + args[0] + " src_addr [src_port [dst_port]]");
+		print("Where src_addr is a valid internet address and src_port and dst_port are valid internet ports.")
+		sys.exit(1);
+
+	proxy = Proxy();
+	proxy.set_option("src_addr", args[1]);
+	if len(args) > 2:
+		proxy.set_option("src_port", args[2]);
+	if len(args) > 3:
+		proxy.set_option("dst_port", args[3]);
+	options = proxy.get_options();
+	print(options["src_addr"] + ":" + str(options["src_port"]) + " --> 0.0.0.0:" + str(options["dst_port"]));
+	try:
+		proxy.run();
+	except KeyboardInterrupt:
+		print("");
+		sys.exit(0);
